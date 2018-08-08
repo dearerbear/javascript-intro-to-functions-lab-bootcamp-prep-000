da878603d3beb9cf7ba748b75469f469ed1e88ff
Dear Bear <dearerbear@gmail.com>
2018-08-08 20:01:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string = string.toLowerCase) {
-    return "I can't hear you!"
-  }
-  if (string = string.toUpperCase) {
-    return "YES INDEED"
-  }
   if (string = "I love you, Grandma") {
     return "I love you, too";
+  } elseif (string = string.toLowerCase) {
+    return "I can't hear you!"
+  } elseif (string = string.toUpperCase) {
+    return "YES INDEED!"
   }
+
 }
