bea3c927aa400b68a012e6199cf8142a2e6c6997
Dear Bear <dearerbear@gmail.com>
2018-08-08 20:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ function sayHiToGrandma(string) {
   } else if (string = string.toUpperCase) {
     return "YES INDEED!"
   }
-  console.log("string");
+  return("string");
 }
+
+console.log(sayHiToGrandma);
 
